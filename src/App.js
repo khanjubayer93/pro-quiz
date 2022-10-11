bee3b485c708cdea56a7517a953013a50c88db54
khanjubayer93 <jubayerkhan711@gmail.com>
2022-10-11 10:12:48 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To The Pro Quiz</h1>
+      <Header></Header>
     </div>
   );
 }
